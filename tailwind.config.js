d68d4393d405fe3a3f6a832de0ee8194c8509a36
adetola-ralph <oreofe.olutola@gmail.com>
2023-07-15 16:24:50 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.{js,ts,jsx,tsx,html}",
+    "./**/*",
+  ],
+  theme: {
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {},
+      fontSize: {},
+    },
+  },
+  plugins: [],
+}
+
